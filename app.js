0ba1e95d2fa7bd1b4bf54899bcd7b9d5bea75835
Dragonkeun <Dragonkeun7@gmail.com>
2023-01-08 20:41:10 +0900

    그리기 모드, 채우기 모드 구현, 지우개 기능 구현

--- app.js
+++ app.js
@@ -1,20 +1,82 @@
+const modeBtn = document.querySelector("#mode-btn");
+const destoryBtn = document.querySelector("#destroy-btn");
+const eraserBtn = document.querySelector("#eraser-btn");
+const lineWidth = document.querySelector("#line-width");
+const color = document.querySelector("#color");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
-const colors = [
-    "red",
-    "blue",
-    "yellow",
-    "orange"
-]
-function onClick(event) {
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    const color = colors[Math.floor(Math.random() * colors.length)]
-    ctx.strokeStyle = color;
+ctx.lineWidth = lineWidth.value;
+let isPainting = false;
+let isFilling = false;
+function onMove(event) {
+  if (isPainting) {
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
+    return;
+  }
+  ctx.beginPath();
+  ctx.moveTo(event.offsetX, event.offsetY);
 }
-canvas.addEventListener("mousemove", onClick)
+function startPainting() {
+  isPainting = true;
+}
+function cancelPainting() {
+  isPainting = false;
+}
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
+function onModeClick() {
+    if (isFilling){
+        isFilling = false;
+        modeBtn.innerText = "채우기 모드로 변경";
+    }
+    else {
+        isFilling = true;
+        modeBtn.innerText = "그리기 모드로 변경";
+    }
+}
+function onCanvasClick(){
+    if(isFilling){
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+function onDestroyClick(){
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function onEraserClick(){
+    ctx.strokeStyle = "#ffffff";
+    isFilling = false; //만약 채우기 모드라면 그리기 모드르 바꿔줌
+    modeBtn.innerText = "채우기 모드로 변경"
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click", onModeClick);
+destoryBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);

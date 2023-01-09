cd871f88902e6bc629eb8701779dcdab7924b029
Dragonkeun <Dragonkeun7@gmail.com>
2023-01-09 23:47:15 +0900

    feat: 배경이미지  추가 기능 구현

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const modeBtn = document.querySelector("#mode-btn");
 const destoryBtn = document.querySelector("#destroy-btn");
 const eraserBtn = document.querySelector("#eraser-btn");
+const fileInput = document.querySelector("#file");
 const lineWidth = document.querySelector("#line-width");
 const color = document.querySelector("#color");
 const colorOptions = Array.from(
@@ -43,32 +44,44 @@ function onColorClick(event) {
   color.value = colorValue;
 }
 function onModeClick() {
-    if (isFilling){
-        isFilling = false;
-        modeBtn.innerText = "채우기 모드로 변경";
-    }
-    else {
-        isFilling = true;
-        modeBtn.innerText = "그리기 모드로 변경";
-    }
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "채우기 모드로 변경";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "그리기 모드로 변경";
+  }
 }
-function onCanvasClick(){
-    if(isFilling){
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-    }
-}
-
-function onDestroyClick(){
-    ctx.fillStyle = "#ffffff";
+function onCanvasClick() {
+  if (isFilling) {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
 }
 
-function onEraserClick(){
-    ctx.strokeStyle = "#ffffff";
-    isFilling = false; //만약 채우기 모드라면 그리기 모드르 바꿔줌
-    modeBtn.innerText = "채우기 모드로 변경"
+function onDestroyClick() {
+  ctx.fillStyle = "#ffffff";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+function onEraserClick() {
+  ctx.strokeStyle = "#ffffff";
+  isFilling = false; //만약 채우기 모드라면 그리기 모드르 바꿔줌
+  modeBtn.innerText = "채우기 모드로 변경";
+}
+
+function onFileChange(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    // drawImage(넣을 이미지, x시작위치, y시작위치, 이미지x크기, 이미지y크기)
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    fileInput.value = null;
+  };
+}
+// canvas.onmousemove = function () {}; 이렇게도 가능
+// canvas.onmousemove = onMove; 이렇게도 가능
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -80,3 +93,4 @@ colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 modeBtn.addEventListener("click", onModeClick);
 destoryBtn.addEventListener("click", onDestroyClick);
 eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
